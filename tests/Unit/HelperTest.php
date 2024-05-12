f7581238d12663e0b7dd16fe628400af870a713d
Sebastiano <sebastiano@racca.me>
2024-05-12 15:55:33 +0200

    Add tests

--- tests/Unit/HelperTest.php
+++ tests/Unit/HelperTest.php
@@ -2,10 +2,19 @@
 
 use Bot\Helper;
 
+it("loads environment variables", function () {
+    Helper::loadDotEnv(ROOT_DIR);
+    $this->assertTrue(isset($_ENV['BOT_TOKEN']));
+});
+
 it("creates a random file", function () {
     $path = Helper::getRandomFilePath();
     $this->assertTrue(file_exists($path));
     $this->assertTrue(is_writable($path));
     $this->assertTrue(is_readable($path));
     $this->assertTrue(unlink($path));
+});
+
+it("removes null values", function () {
+    $this->assertTrue(count(Helper::arrayFilter([0, 1, null, 2])) === 3);
 });
